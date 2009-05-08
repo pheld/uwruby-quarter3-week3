9ed1a80d28d163f4410dc0ccd32c9daf8b05dbba
Peter Held <peter.t.held@gmail.com>
2009-05-07 22:22:37 -0700

    Multi-threaded!

--- lib/worker_bee.rb
+++ lib/worker_bee.rb
@@ -1,25 +1,13 @@
+# Peter Held
+# Homework 3
+require 'thread'
+
 class WorkerBee
-  # subclass to hold work tasks
-  class Work
-    attr_reader :dependencies
-    attr_reader :block
-
-    def initialize(dependencies, block)
-      @dependencies = dependencies
-      @block = block
-    end
-  end
-
-  @@work_items = Hash.new
-  @@work_done = Hash.new
-  @@indent = 0
+  
+  @@work_items = {}
 
   def self.work_items
     @@work_items
-  end
-
-  def self.work_done
-    @@work_done
   end
 
   def self.recipe &block
@@ -33,49 +21,52 @@ class WorkerBee
     @@work_items[name] = new_task 
   end
 
-  def self.run item_name
+  def self.run (item_name, work_done={}, indent=0)
     work_item = @@work_items[item_name]
+    indent_string = "  " * indent
 
-    puts "running #{item_name}"
- 
-    # run the dependencies first, in order of definition
-    unless work_item.dependencies.nil?
-      
+    unless work_done.has_key?(item_name)
+      threads = []
+
+      print "#{indent_string}running #{item_name}\n"
+
+      # run the dependencies first, in order of definition
       work_item.dependencies.each do |dependency_name|
-        
-        # status reporting indenting
-        @@indent += 1 
-        (@@indent * 2).times do
-          putc " "
-        end
-
-        # run the dependency if it hasn't been run before
-        puts "dependency: #{dependency_name} = #{@@work_done[dependency_name].nil?}"
-        if @@work_done[dependency_name].nil?
-          run(dependency_name)
-        else
-          puts "not running #{dependency_name} - already met dependency"
-        end
-
+        threads << Thread.new {
+          # run the dependency if it hasn't been run before
+          unless work_done[dependency_name] then
+            run(dependency_name, work_done, indent + 1)
+          else
+            print "#{indent_string}  not running #{dependency_name} - already met dependency\n"
+          end
+        }
       end
 
-    end
-
-    # return the indent to what it was before the dependencies were run
-    @@indent = @@indent - work_item.dependencies.length 
-
-    # increment the number of times this work item has been run
-    puts "work: #{item_name} = #{@@work_done[item_name].nil?}"
-    if @@work_done[item_name].nil?
-      puts "did #{item_name}!!!!!!"
-      @@work_done[item_name] = 1
-      puts "AAAAAAND...#{@@work_done[item_name].nil?}"
-    else
-      puts "re-did #{item_name}!!!!!"
-      @@work_done[item_name] += 1 # increment the execution count for this item
+      threads.each { |thread| thread.join }
     end
 
     # run the work item itself
     instance_eval(&work_item.block)
+    
+    work_done[item_name] = 1
+    print "#{indent_string}#{item_name} DONE\n"
+
+    # return the work done
+    work_done
   end
+
+  # subclass to hold work tasks
+  class Work
+    attr_reader :dependencies
+    attr_reader :block
+
+    def initialize(dependencies, block)
+      @dependencies = dependencies
+      @block = block
+    end
+  end
+
 end
+
+recipe = ARGV
+WorkerBee.run recipe if $0 == __FILE__
--- test/test_worker_bee.rb
+++ test/test_worker_bee.rb
@@ -1,31 +1,26 @@
+# Peter Held
+# Homework 3
+
 require 'test/unit'
 require 'worker_bee'
 
 class TestWorkerBee < Test::Unit::TestCase
-  def setup
-    # @bee = WorkerBee.new
-  end
 
-  def test_adds_work_to_hash
-#    puts WorkerBee.work_items[0]
+ def test_adds_work_to_hash
     assert_nil(WorkerBee.work_items[:test_task])    
     
     WorkerBee.work :test_task, :dependency_1, :dependency_2 do
-      puts "this is the code for 'test_task'"
+      "test_task"
     end
     
     assert_not_nil(WorkerBee.work_items[:test_task])
 
-    WorkerBee.work :test_task_2, :dependency_2 do
-      puts "this is the code for 'test_task_2'"
-    end
-
-    assert_not_nil(2, WorkerBee.work_items[:test_task_2])
-  end 
+    assert_equal(2, WorkerBee.work_items[:test_task].dependencies.length)
+ end 
 
   def test_add_dependencies_to_work
     WorkerBee.work :test_task_3, :dependency_1, :dependency_2 do
-      puts "this is the code for 'test_task_3'"
+      "test_task_3"
     end
 
     # check that there are two dependencies on the new work
@@ -51,9 +46,9 @@ class TestWorkerBee < Test::Unit::TestCase
       end
     end
 
-    return_val = WorkerBee.run :test_work_item
+    work_done = WorkerBee.run :test_work_item
 
-    assert_equal("return value", return_val)
+    assert_not_nil(work_done[:test_work_item])
   end 
 
   def test_dependencies_get_run
@@ -71,33 +66,47 @@ class TestWorkerBee < Test::Unit::TestCase
       end
     end
 
-    WorkerBee.run :test_work_item
+    work_done = WorkerBee.run :test_work_item
 
-    assert_not_nil(WorkerBee.work_done[:test_work_item])
-    assert_not_nil(WorkerBee.work_done[:dependency_1])
-    assert_not_nil(WorkerBee.work_done[:dependency_2])
+    assert_not_nil(work_done[:test_work_item])
+    assert_not_nil(work_done[:dependency_1])
+    assert_not_nil(work_done[:dependency_2])
   end
 
   def test_previously_run_dependencies_get_skipped
     WorkerBee.recipe do
-      work :test_work_item, :already_run_dependency, :regular_dependency do
+      work :test_work_item, :already_run_dependency, :regular_dependency, :dependency_foo, :dependency_long, :dependency_short do
         "test_work_item return value"
       end
 
-      work :regular_dependency, :arleady_run_dependency do
+      work :regular_dependency, :already_run_dependency do
         "regular_dependency return value"
       end
 
       work :already_run_dependency do
         "already_run_dependency return value"
       end
+      
+      work :dependency_foo do
+        "dependency_foo return value"
+      end
 
-      WorkerBee.run :test_work_item
+      work :dependency_long do
+        sleep(3)
+      end
 
-      # asser that everything got run once each (no double task execution)
-      assert_equal(1, WorkerBee.work_done[:test_work_item])
-      assert_equal(1, WorkerBee.work_done[:already_run_dependency])
-      assert_equal(1, WorkerBee.work_done[:regular_dependency])
+      work :dependency_short do
+        sleep(1)
+      end
+
     end
+    
+    work_done = WorkerBee.run :test_work_item
+
+    # asser that everything got run once each (no double task execution)
+    assert_equal(1, work_done[:test_work_item])
+    assert_equal(1, work_done[:already_run_dependency])
+    assert_equal(1, work_done[:regular_dependency])
+
   end
 end
